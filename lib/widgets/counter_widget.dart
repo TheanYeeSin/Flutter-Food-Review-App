d5e3d631d4107409859278cfc566c10790fdb07f
Thean Yee Sin <yeesinxx@gmail.com>
2023-09-03 19:45:29 +0800

    Add a total review counter on main screen

--- lib/widgets/counter_widget.dart
+++ lib/widgets/counter_widget.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:foodreviewapp/database/database_service.dart';
 
 class CounterWidget extends StatefulWidget {
-  final String columnName;
-  final String columnValue;
+  final String? columnName;
+  final String? columnValue;
+  final int? count;
+
   const CounterWidget(
-      {super.key, required this.columnName, required this.columnValue});
+      {super.key, this.columnName, this.columnValue, this.count});
 
   @override
   State<CounterWidget> createState() => _CounterWidgetState();
@@ -15,8 +17,10 @@ class _CounterWidgetState extends State<CounterWidget> {
   int reviewCount = 0;
 
   _countReview() async {
-    var reviews = await DatabaseService.getReviewsByColumn(
-        widget.columnName, widget.columnValue);
+    var reviews = widget.columnName != null && widget.columnValue != null
+        ? await DatabaseService.getReviewsByColumn(
+            widget.columnName!, widget.columnValue!)
+        : await DatabaseService.getAllReviews();
     setState(() {
       reviewCount = reviews?.length ?? 0;
     });
@@ -25,19 +29,27 @@ class _CounterWidgetState extends State<CounterWidget> {
   @override
   void initState() {
     super.initState();
-    _countReview();
+    if (widget.count != null) {
+      setState(() {
+        reviewCount = widget.count!;
+      });
+    } else {
+      _countReview();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
         margin: const EdgeInsets.only(right: 5.0, left: 5.0),
-        padding: const EdgeInsets.all(3.0),
+        padding:
+            const EdgeInsets.only(top: 2.0, bottom: 2.0, left: 5.0, right: 5.0),
         decoration: BoxDecoration(
-            border: Border.all(color: Colors.grey[400]!),
+            border: Border.all(color: Colors.grey[600]!),
             borderRadius: BorderRadius.circular(5.0),
-            color: Colors.grey[200]),
-        child:
-            Text('$reviewCount', style: const TextStyle(color: Colors.black)));
+            color: Colors.grey[400]),
+        child: Text('$reviewCount',
+            style: const TextStyle(
+                color: Colors.black, fontWeight: FontWeight.bold)));
   }
 }
